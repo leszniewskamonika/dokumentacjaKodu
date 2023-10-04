4315d022641b4eacad52ea092e3493be90464f4c
Monika Leszniewska <monia.leszniewska@gmail.com>
2023-10-04 18:39:14 +0200

    Created folder for test programs

---
+++ baza/room.py
@@ -0,0 +1,8 @@
+class Room:
+    length = 0.0
+    breadth = 0.0
+
+    def calculate_area(self):
+        print("Area of Room = ", self.length*self.breadth)
+
+
--- room.py
+++
@@ -1,8 +0,0 @@
-class Room:
-    length = 0.0
-    breadth = 0.0
-
-    def calculate_area(self):
-        print("Area of Room = ", self.length*self.breadth)
-
-
